fad678857cd801a80c32f6ac1dafcb7558cc74d1
Amazing-create2 <142730588+Amazing-create2@users.noreply.github.com>
2023-09-05 00:55:27 +0530

    Add files via upload

--- version.py
+++ version.py
@@ -1,58 +1,15 @@
-# Copyright (C) Dnspython Contributors, see LICENSE for text of ISC license
-
-# Copyright (C) 2003-2017 Nominum, Inc.
+# Copyright The OpenTelemetry Authors
 #
-# Permission to use, copy, modify, and distribute this software and its
-# documentation for any purpose with or without fee is hereby granted,
-# provided that the above copyright notice and this permission notice
-# appear in all copies.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# THE SOFTWARE IS PROVIDED "AS IS" AND NOMINUM DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL NOMINUM BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
-# OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-"""dnspython release version information."""
-
-#: MAJOR
-MAJOR = 2
-#: MINOR
-MINOR = 4
-#: MICRO
-MICRO = 0
-#: RELEASELEVEL
-RELEASELEVEL = 0x0F
-#: SERIAL
-SERIAL = 0
-
-if RELEASELEVEL == 0x0F:  # pragma: no cover  lgtm[py/unreachable-statement]
-    #: version
-    version = "%d.%d.%d" % (MAJOR, MINOR, MICRO)  # lgtm[py/unreachable-statement]
-elif RELEASELEVEL == 0x00:  # pragma: no cover  lgtm[py/unreachable-statement]
-    version = "%d.%d.%ddev%d" % (
-        MAJOR,
-        MINOR,
-        MICRO,
-        SERIAL,
-    )  # lgtm[py/unreachable-statement]
-elif RELEASELEVEL == 0x0C:  # pragma: no cover  lgtm[py/unreachable-statement]
-    version = "%d.%d.%drc%d" % (
-        MAJOR,
-        MINOR,
-        MICRO,
-        SERIAL,
-    )  # lgtm[py/unreachable-statement]
-else:  # pragma: no cover  lgtm[py/unreachable-statement]
-    version = "%d.%d.%d%x%d" % (
-        MAJOR,
-        MINOR,
-        MICRO,
-        RELEASELEVEL,
-        SERIAL,
-    )  # lgtm[py/unreachable-statement]
-
-#: hexversion
-hexversion = MAJOR << 24 | MINOR << 16 | MICRO << 8 | RELEASELEVEL << 4 | SERIAL
+__version__ = "1.18.0"
